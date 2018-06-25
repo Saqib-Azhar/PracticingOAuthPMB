1fd58ddc878a7a3401822e1cddd4e9c4334c99d3
sazhar <sazhar@PK-SAZHAR>
2018-06-25 16:01:07 +0500

    Add project files.

---
+++ Practicing_OAuth/Controllers/HomeController.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.IO;
+using Practicing_OAuth;
+using System.Web.Configuration;
+using Practicing_OAuth.Models;
+
+namespace Practicing_OAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static string SenderEmailId = WebConfigurationManager.AppSettings["DefaultEmailId"];
+        private static string SenderEmailPassword = WebConfigurationManager.AppSettings["DefaultEmailPassword"];
+        private static int SenderEmailPort = Convert.ToInt32(WebConfigurationManager.AppSettings["DefaultEmailPort"]);
+        private static string SenderEmailHost = WebConfigurationManager.AppSettings["DefaultEmailHost"];
+        private Practicing_OAuthEntities db = new Practicing_OAuthEntities();
+        public ActionResult Index()
+        {
+            var ProductsList = db.Products.ToList();
+            ViewBag.ProductsList = ProductsList;
+            ViewBag.CategoriesList = db.Categories.ToList();
+            ViewBag.CategoryTypesList = db.CategoryTypes.ToList();
+            return View();
+        }
+
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult PriceQuote()
+        {
+            return View();
+        }
+        public ActionResult ContactDetails(FormCollection contactDetails)
+        {
+            var Name = contactDetails["Name"];
+            var Email = contactDetails["Email"];
+            var Phone = contactDetails["Phone"];
+            var Message = contactDetails["Message"];
+
+            try
+            {
+                ContactUsData ContactUsObject = new ContactUsData();
+
+                ContactUsObject.UserName = Name;
+                ContactUsObject.Email = Email;
+                ContactUsObject.PhoneNumber = Phone;
+                ContactUsObject.Message = Message;
+                ContactUsObject.IsDeleted = false;
+                ContactUsObject.Seen = false;
+                ContactUsObject.SeenTime = null;
+                ContactUsObject.SubmittedTime = DateTime.Now;
+
+
+                db.ContactUsDatas.Add(ContactUsObject);
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            { }
+          
+
+
+            var fromAddress = new MailAddress(SenderEmailId, "Contact Message By: " + Name);
+            var toAddress = new MailAddress(SenderEmailId, "Print My Box");
+            string fromPassword = SenderEmailPassword;
+            string subject = "PrintMyBox Contact Us form Submission by: " + Name;
+            string body = "Name: " + Name + "<br>Phone: " + Phone + "<br>" + "Email: " + Email + "<br>" + "Message: " + Message + "<br>Time: " + DateTime.Now;
+
+            var smtp = new SmtpClient
+            {
+                Host = SenderEmailHost,
+                Port = SenderEmailPort,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                IsBodyHtml = true,
+                Subject = subject,
+                Body = body,
+
+            })
+            {
+                //message.Bcc.Add("support@printmybox.com");
+                smtp.Send(message);
+            }
+
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            var fromSiteAddress = new MailAddress(SenderEmailId, "Print My Box");
+            var toCustomerAddress = new MailAddress(Email, Name);
+            string fromSitePassword = SenderEmailPassword;
+            subject = "PrintMyBox: Your Contact Form has submitted, Thankyou for your precious time ";
+            body = " <b>Form</b><br>Name: " + Name + "<br>Phone: " + Phone + "<br>" + "Email: " + Email + "<br>" + "Message: " + Message + "<br>Time: " + DateTime.Now;
+
+            var smtp1 = new SmtpClient
+            {
+                Host = SenderEmailHost,
+                Port = SenderEmailPort,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromSiteAddress.Address, fromSitePassword),
+                Timeout = 20000
+            };
+            using (var message = new MailMessage(fromSiteAddress, toCustomerAddress)
+            {
+                IsBodyHtml = true,
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp1.Send(message);
+            }
+
+
+            return RedirectToAction("Contact");
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult QuoteRequest(FormCollection fc, HttpPostedFileBase File_input)/*string ProductName, string Name, string Email, string Phone, string Stock, string Color, string Quantity, string Height, string Width, string Depth, string File, string Comments*/
+        {
+            try
+            {
+                string ProductName = fc["ProductName-input"];
+                string Name = fc["Name-input"];
+                string Email = fc["Email-input"];
+                string Phone = fc["Phone-input"];
+                string Stock = fc["Stock-input"];
+                string Color = fc["Color-input"];
+                string Quantity = fc["Quantity-input"];
+                string Height = fc["Height-input"];
+                string Width = fc["Width-input"];
+                string Depth = fc["Depth-input"];
+                string Comments = fc["Comments-input"];
+                string ViewPath = fc["ViewPath"];
+                HttpPostedFileBase AttachmentUserCopy = File_input;
+
+                try
+                {
+                    PriceQuote QuoteObject = new PriceQuote();
+                    if (AttachmentUserCopy != null)
+                    {
+                        string pic = System.IO.Path.GetFileName(AttachmentUserCopy.FileName);
+                        string path = System.IO.Path.Combine(
+                                               Server.MapPath("~/UploadedProductImages"), pic);
+                        // file is uploaded
+                        AttachmentUserCopy.SaveAs(path);
+
+                        // save the image path path to the database or you can send image 
+                        // directly to database
+                        // in-case if you want to store byte[] ie. for DB
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            AttachmentUserCopy.InputStream.CopyTo(ms);
+                            byte[] array = ms.GetBuffer();
+                        }
+                        QuoteObject.File = AttachmentUserCopy.FileName;
+
+                    }
+                    QuoteObject.UserName = Name;
+                    QuoteObject.Email = Email;
+                    QuoteObject.PhoneNumber = Phone;
+                    QuoteObject.Quantity = Convert.ToInt32(Quantity);
+                    QuoteObject.Stock = Stock;
+                    QuoteObject.Color = Color;
+                    QuoteObject.Height = Convert.ToInt32(Height);
+                    QuoteObject.Width = Convert.ToInt32(Width);
+                    QuoteObject.Depth = Convert.ToInt32(Depth);
+                    QuoteObject.Comments = Comments;
+                    QuoteObject.SubmittedTime = DateTime.Now;
+                    QuoteObject.IsDeleted = false;
+                    QuoteObject.SeenTime = null;
+                    QuoteObject.Seen = false;
+
+                    db.PriceQuotes.Add(QuoteObject);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                { }
+                
+
+
+                var fromAddress = new MailAddress(SenderEmailId, "Quote Request: " + Name);
+                var toAddress = new MailAddress(Email, "Quote Request" + Name);
+                string fromPassword = SenderEmailPassword;
+                string subject = "Quote Request for Product " + ProductName;
+                string body = "Name: " + Name + "<br>Phone: " + Phone + "<br>" + "Email: " + Email + "<br>" + "Product Details: " + ProductName + "<br>Stock: " + Stock + "<br>Color: " + Color + "<br>Quantity: " + Quantity + "<br>Dimensions: " + Height + "x" + Width + "x" + Depth + "<br>Comments: " + Comments + "<br>Time: " + DateTime.Now;
+
+                var smtp = new SmtpClient
+                {
+                    Host = SenderEmailHost,
+                    Port = SenderEmailPort,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                    Timeout = 20000
+                };
+
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    IsBodyHtml = true,
+                    Subject = subject,
+                    Body = body
+                })
+                {
+
+                    if (File_input != null)
+
+                    {
+
+                        string fileName = Path.GetFileName(File_input.FileName);
+                        File_input.InputStream.Seek(0, SeekOrigin.Begin);
+                        message.Attachments.Add(new Attachment(File_input.InputStream, fileName, MediaTypeNames.Application.Octet));
+                        message.Bcc.Add(SenderEmailId);
+                    }
+
+                    smtp.Send(message);
+                }
+
+
+                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+                // var fromSiteAddress = new MailAddress(SenderEmailId, "Print My Box");
+                //var toCustomerAddress = new MailAddress(Email, Name);
+                //string fromSitePassword = SenderEmailPassword;
+                //subject = "Quote Request Confirmation";
+                //body = "Your Following Quote Request Submitted, Thankyou for your precious time <br><br><b>Quote Request</b><br>Name: " + Name + "<br>Phone: " + Phone + " <br> " + "Email: " + Email + " <br> " + "Product Details: " + ProductName + " <br> Stock: " + Stock + " <br> Color: " + Color + " <br> Quantity: " + Quantity + " <br> Dimensions: " + Height +"x"+ Width +"x" + Depth + " <br> Time: " + DateTime.Now;
+                //HttpPostedFileBase File = null;
+                //if (Request.Files.Count > 0)
+                //{
+                //    File = Request.Files[0];
+                //}
+
+                //var smtp1 = new SmtpClient
+                //{
+                //    Host = SenderEmailHost,
+                //    Port = SenderEmailPort,
+                //    EnableSsl = true,
+                //    DeliveryMethod = SmtpDeliveryMethod.Network,
+                //    Credentials = new NetworkCredential(fromSiteAddress.Address, fromSitePassword),
+                //    Timeout = 20000
+                //};
+                //using (var message1 = new MailMessage(fromSiteAddress, toCustomerAddress)
+                //{
+                //    IsBodyHtml = true,
+                //    Subject = subject,
+                //    Body = body
+                //})
+                //{
+
+                //    if (AttachmentUserCopy != null)
+
+                //    {
+
+                //        string fileName = Path.GetFileName(AttachmentUserCopy.FileName);
+                //        AttachmentUserCopy.InputStream.Seek(0, SeekOrigin.Begin);
+                //        message1.Attachments.Add(new Attachment(AttachmentUserCopy.InputStream, fileName, MediaTypeNames.Application.Octet));
+
+                //    }
+                //    smtp1.Send(message1);
+                //}
+
+
+                //return new FilePathResult(ViewPath, "text/html");
+                var smtp1 = new SmtpClient
+                {
+                    Host = SenderEmailHost,
+                    Port = SenderEmailPort,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                    Timeout = 20000
+                };
+
+                using (var message1 = new MailMessage(SenderEmailId, SenderEmailId)
+                {
+                    IsBodyHtml = true,
+                    Subject = subject,
+                    Body = body
+                })
+                {
+
+                    if (File_input != null)
+
+                    {
+
+                        string fileName = Path.GetFileName(File_input.FileName);
+                        File_input.InputStream.Seek(0, SeekOrigin.Begin);
+                        message1.Attachments.Add(new Attachment(File_input.InputStream, fileName, MediaTypeNames.Application.Octet));
+
+                    }
+
+                    smtp1.Send(message1);
+                }
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Error");
+                //return Json("Something Went Wrong!", JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practicing_OAuth/Controllers/ProductsController.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practicing_OAuth.Models;
+using System.IO;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Practicing_OAuth.Controllers
+{
+    public class ProductsController : Controller
+    {
+        class searchClass
+        {
+            public List<Product> searchedProdList { get; set; }
+            public string searchedQuery { get; set; }
+        }
+        private Practicing_OAuthEntities db = new Practicing_OAuthEntities();
+        private static List<Product> ProductsList = new List<Product>();
+        private static searchClass searchObjectsList = new searchClass();
+
+        [Authorize(Roles = "Admin")]
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+          Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            var Categories = db.Categories.Where(s => s.Id == product.CategoryId).ToList();
+            ViewBag.CategoriesList = Categories;
+            var Products = db.Products.Where(s => s.CategoryId == product.CategoryId).ToList();
+            ViewBag.ProductsList = Products;
+            return View(product);
+        }
+
+        public ActionResult Search(string query, int? pageNo = 1, int? pageSize = 16)
+        {
+            try
+            {
+                if (searchObjectsList.searchedProdList == null || (searchObjectsList.searchedProdList.Count == 0 || searchObjectsList.searchedQuery != query))
+                {
+                    searchObjectsList.searchedProdList = db.Products.Where(s => s.Name.Contains(query) && s.IsEnabled == true).ToList();
+                    searchObjectsList.searchedQuery = query;
+                }
+                ViewBag.QueryString = query;
+                return View(searchObjectsList.searchedProdList.ToPagedList(Convert.ToInt32(pageNo), Convert.ToInt32(pageSize)));
+
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("NoResultFound","Products",query);
+            }
+        }
+        public ActionResult NoResultFound(string query)
+        {
+            ViewBag.QueryString = query;
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName");
+            return View();
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Price,SlugURL,Description,IsEnabled,Image1,Image2,Image3,Image4,Image5,UploadedDate,CategoryId")] Product product, HttpPostedFileBase Image1, HttpPostedFileBase Image2, HttpPostedFileBase Image3, HttpPostedFileBase Image4, HttpPostedFileBase Image5)
+        {
+            if (Image1 != null)
+            {
+                string pic = System.IO.Path.GetFileName(Image1.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/UploadedProductImages"), pic);
+                // file is uploaded
+                Image1.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Image1.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+                product.Image1 = Image1.FileName;
+
+            }
+            if (Image2 != null)
+            {
+                string pic = System.IO.Path.GetFileName(Image2.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/UploadedProductImages"), pic);
+                // Image2 is uploaded
+                Image2.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Image2.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+                product.Image2 = Image2.FileName;
+
+            }
+            if (Image3 != null)
+            {
+                string pic = System.IO.Path.GetFileName(Image3.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/UploadedProductImages"), pic);
+                // Image3 is uploaded
+                Image3.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Image3.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+                product.Image3 = Image3.FileName;
+
+            }
+            if (Image4 != null)
+            {
+                string pic = System.IO.Path.GetFileName(Image4.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/UploadedProductImages"), pic);
+                // Image4 is uploaded
+                Image4.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Image4.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+                product.Image4 = Image4.FileName;
+            }
+            if (Image5 != null)
+            {
+                string pic = System.IO.Path.GetFileName(Image5.FileName);
+                string path = System.IO.Path.Combine(
+                                       Server.MapPath("~/UploadedProductImages"), pic);
+                // Image5 is uploaded
+                Image5.SaveAs(path);
+
+                // save the image path path to the database or you can send image 
+                // directly to database
+                // in-case if you want to store byte[] ie. for DB
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    Image5.InputStream.CopyTo(ms);
+                    byte[] array = ms.GetBuffer();
+                }
+
+                product.Image5 = Image5.FileName;
+            }
+            if (ModelState.IsValid)
+            {
+                product.SlugURL = product.Name.Replace(" ", "_").ToLower();
+                product.UploadedDate = DateTime.Now;
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Redirect("/Products/Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Price,SlugURL,Description,IsEnabled,Image1,Image2,Image3,Image4,Image5,UploadedDate,CategoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return Redirect("/Products/Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+        [Authorize(Roles = "Admin")]
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [Authorize(Roles = "Admin")]
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return Redirect("/Products/Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult productsByCategoryType(int id, int? pageNo = 1, int? pageSize = 16)
+        {
+            if (ProductsList.Count == 0)
+            {
+                var products = db.Products.ToList();
+                ProductsList = products;
+            }
+            ViewBag.Id = id;
+            var ProdSubList = ProductsList.Where(s => s.Category.CategoryTypeId == id && s.IsEnabled == true);
+            return View(ProdSubList.ToPagedList(Convert.ToInt32(pageNo), Convert.ToInt32(pageSize)));
+
+        }
+        public ActionResult productsByCategory(int id, int? pageNo = 1, int? pageSize = 16)
+        {
+            if (ProductsList.Count == 0)
+            {
+                var products = db.Products.ToList();
+                ProductsList = products;
+            }
+            ViewBag.Id = id;
+            var ProdSubList = ProductsList.Where(s => s.CategoryId == id && s.IsEnabled == true);
+            return View(ProdSubList.ToPagedList(Convert.ToInt32(pageNo), Convert.ToInt32(pageSize)));
+        }
+
+        public ActionResult AllProducts(int? pageNo = 1,int? pageSize = 16)
+        {
+            if (ProductsList.Count == 0)
+            {
+                var products = db.Products.ToList();
+                ProductsList = products;
+            }
+            return View(ProductsList.ToPagedList(Convert.ToInt32(pageNo), Convert.ToInt32(pageSize)));
+        }
+    }
+}
